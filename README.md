36cd20b566a0fd173fc80a9b084555dbec2dde26
mcbees <mcbeesteelix83@gmail.com>
2025-01-23 09:35:26 -0700

    Finalized mac.sh and README. Will review once more

--- README.md
+++ README.md
@@ -8,16 +8,16 @@
  - One is uniquely assigned to each NIC (Network Interface Card) at production.
  - It holds six 8-bit numbers (*base 16 number pairs*), formatted like so: 
 
-    *`02:46:8A:CE:15:F9`*
+    *`02:46:8A:CE:15:F9`* (Case insensitive)
  - They are neccessary for communication on the local network.
 ---
 ### There are limitations, however. 
 
 When spoofing a MAC address, the first 8-bit number (**00**:00:00:00:00:00) is highly regulated.
 
-To keep safe:
-* It may not be an odd number (ending in 1,3,5,7,9,B,D, or F)
-* It may not end in a 0 or an 8 (58, F0, A8, 10, etc.)
+To keep safe, it may not end in:
+* An odd number (ending in 1,3,5,7,9,B,D, or F)
+* Either a 0 or an 8 (58, F0, A8, 10, etc.)
 
 ## Usage
 
@@ -27,12 +27,30 @@ To keep safe:
 
 3. You can run the function using `./mac.h 'Interface' 'NewMacAddress'`, the new Mac address being whatever you want to spoof it to.
 
-## User Feedback
+4. If you replace your 'NewMacAddress' input with `default`, your MAC address will be set back to factory default.
 
-An error will occur when:
+5. **Similary**, you can use `random` to generate a random MAC address, which you can choose to use or not. The first digits will always be `02` in order to comply with limitations.
+
+## System Feedback
+
+
+
+An ERROR will occur when:
 
 1. Inputting an **interface** that is **not detected** on your local newtork.
 
-2. Inputting a **MAC address** that **does not follow standard format**: `00:00:00:00:00:00`
+2. Trying to use the **`default` function** while the MAC address is **already factory default**.
 
-3.
+3. Inputting a **MAC address** that **does not follow standard format**: *`XX:XX:XX:XX:XX:XX`* (case insensitive)
+
+4. Using the **`random` function**, but **not correctly confirming** with either Y or YES (case insensitive)
+
+### Address Record:
+When switching to a **New MAC Address**, the **Initial MAC Address**, is automatically recorded along with its **Interface** in the *`./history.txt`*  file.
+
+### Final:
+If a command follows through completely, you should see your selected *Interface*, the *Initial Address*, and a *Success!* message showing your *New Address*
+
+---
+
+Happy spoofing!
--- mac.sh
+++ mac.sh
@@ -1,23 +1,22 @@
 #Take 2 inputs
 INTERFACE=$1
 NEWADDY=$2
+
+#if no such INTERFACE is detected by the device throw error
 if ip link show | grep -q  "$INTERFACE:"; then
-    echo "Interface: $INTERFACE"
+    echo "Interface: $INTERFACE" 
 else
     echo "Error: Invalid Interface"
     exit 1
 fi
 
-#this function looks at the ips but only grabs the info from the specified interface, the info is then filtered to only grab mac addresses. Since there are going to be 2, head only takes the first one.
+#this function looks at the addresses but only grabs the info from the specified interface. 
+#the info is then filtered to only grab mac addresses. Since there will be atleast 2, head only takes the first one.
 ADDY=$(ip link show $INTERFACE| grep -oE -m1 '([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}' | head -n 1)
 
-echo "Initial Address: $ADDY"
-echo "$ADDY" >> ./history.txt
 
-
-#check if new address is in a valid format
+#if default, check that address is not already set to its default
 if [[ "$NEWADDY" == "default" ]]; then
-
     if [[ -n $(ip link show $INTERFACE | grep -oE -m1 'permaddr ([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}' | awk '{print $2}') ]]; then
         NEWADDY=$(ip link show $INTERFACE | grep -oE -m1 'permaddr ([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}' | awk '{print $2}')
     else
@@ -25,15 +24,42 @@ if [[ "$NEWADDY" == "default" ]]; then
         exit 2
     fi
 
+    #if random, create a random address and set a variable which will prompt user confirmation before randomly setting the address
+elif [[ "$NEWADDY" == "random" ]]; then
+    NEWADDY=$(printf "02:%02x:%02x:%02x:%02x:%02x" $((RANDOM % 256)) $((RANDOM % 256)) $((RANDOM % 256)) $((RANDOM % 256)) $((RANDOM % 256)))
+    CONFIRM=lol
+
+    #if trying to put in an address, it must in proper xx:xx:xx:xx:xx:xx format
 elif [[ ! "$NEWADDY" =~ ^([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}$ ]]; then
     echo "Error: '$NEWADDY' is not a valid address" >&2
     exit 3
 fi
-    
-    sudo ip link set "$INTERFACE" address "$NEWADDY"
-    echo "Success! New Address: $NEWADDY"
-    exit 0
+
+#System feedback
+echo "Initial Address: $ADDY"
+
+if [[ -n $CONFIRM ]]; then
+    read -p "Are you sure you want to change your MAC address to $NEWADDY? (Type Y or YES to confirm): " CONFIRM2
+    if [[ ! ( ${CONFIRM2,,} == "yes" || ${CONFIRM2,,} == "y" ) ]]; then
+        echo "Confirmation Failed"
+        exit 4
+    fi
+fi
+
+
+
+#if everything so far has gone right, the function will not have exited and the MAC switch can be made.
+
+#if the MAC address has not been recorded, place it in history.
+if [[ -z $(grep "$INTERFACE | $ADDY" ./history.txt) ]]; then
+    echo "$INTERFACE | $ADDY" >> ./history.txt
+fi
+
+#Congrats! You have a new MAC address
+sudo ip link set "$INTERFACE" address "$NEWADDY"
+echo "Success! New Address: $NEWADDY"
+
 
 #create randomization for first 8-bit number or just use 02:
-    #first number cannot be odd and first number cannot end in 0 or 8. (eg. 00, 08, F8, 20, 38, etc.)
+
 #create randomization function for last 5 numbers
