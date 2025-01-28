5686503b78ee5509cd2a1a24abbb4224c7bf5244
mcbees <mcbeesteelix83@gmail.com>
2025-01-28 11:00:58 -0700

    1/28/25

--- ipf.py
+++ ipf.py
@@ -13,15 +13,30 @@ def function():
     INPUT = sys.argv[1]
 
     if not re.search(r"^(\d{1,3}[.]){3}\d{1,3}[/]\d{1,2}$", INPUT):
-        print(f"ERROR: Invalid IP format. Correct format: \n\n``` \n '0-255'.'0-255'.'0-255'.'0-255'/'1-32' \n                                     ```")
+        print(f"ERROR: Invalid IP format. Correct format: \n\n``` \n '255'.'255'.'255'.'255'/'32' \n                                     ```")
         return 2
 
     network = ipaddress.ip_network(INPUT, strict=False)
 
-    for ip in network.hosts():
-        print(ip)
-        output = subprocess.run(['ping', f"{ip}", '-c', '1', '-W', '.05'])#, text=True capture_output=True ) ### Send to stdout & stderr
+    DOWN = 0
+    UP = 0
+    COUNT = 0
+
+    for ip in network.hosts(): #only send 1 packet and only wait 10ms for a response   #captures output as byte string and turns it into text
+        PING = subprocess.run(['ping', f'{ip}', '-c', '1', '-W', '.01'], text=True, capture_output=True) 
+        output = PING.stdout
+        if '0 received' not in PING.stdout:
+            print(PING.stdout)
+        else:
+            print(ip)
+        if PING.returncode == 0:
+            UP += 1
+        else:
+            DOWN += 1
         
+        
+        COUNT += 1
+    print(f"{UP} + {DOWN} = {COUNT}")
         ### Detect if network is UP or DOWN
 
 function()
